14db9dd70b26b14f527a1a1d8bf5a21c78c1614d
Ticketedmoon <shane.creedon3@gmail.com>
2023-12-03 16:50:51 +0000

    feature/line-segmentation: Convert lineStrip to Quad for player and walls
    Signed-off-by: Ticketedmoon <shane.creedon3@gmail.com>

--- src/scene/gameplay_scene/entity/entity_spawner.cpp
+++ src/scene/gameplay_scene/entity/entity_spawner.cpp
@@ -12,12 +12,12 @@ void EntitySpawner::spawnPlayer()
     Vec2 position{Crucible::WINDOW_WIDTH / 2.0f, Crucible::WINDOW_HEIGHT - 128};
     Vec2 dimensions{50, 50};
 
-    sf::VertexArray vertices(sf::LineStrip, 5);
-    vertices[0].position = sf::Vector2f(position.x - dimensions.x/2, position.y - dimensions.y/2);
-    vertices[1].position = sf::Vector2f(position.x + dimensions.x/2, position.y - dimensions.y/2);
-    vertices[2].position = sf::Vector2f(position.x + dimensions.x/2, position.y + dimensions.y/2);
-    vertices[3].position = sf::Vector2f(position.x - dimensions.x/2, position.y + dimensions.y/2);
-    vertices[4].position = sf::Vector2f(position.x - dimensions.x/2, position.y - dimensions.y/2);
+    sf::VertexArray vertices(sf::Quads, 5);
+    vertices[0] = sf::Vertex({position.x - dimensions.x/2, position.y - dimensions.y/2}, sf::Color::Green);
+    vertices[1] = sf::Vertex({position.x + dimensions.x/2, position.y - dimensions.y/2}, sf::Color::Green);
+    vertices[2] = sf::Vertex({position.x + dimensions.x/2, position.y + dimensions.y/2}, sf::Color::Green);
+    vertices[3] = sf::Vertex({position.x - dimensions.x/2, position.y + dimensions.y/2}, sf::Color::Green);
+    vertices[4] = sf::Vertex({position.x - dimensions.x/2, position.y - dimensions.y/2}, sf::Color::Green);
 
     sf::VertexArray rayVertices(sf::LineStrip, 2);
     rayVertices[0] = sf::Vertex({position.x, position.y - dimensions.y / 2}, sf::Color::Yellow);
@@ -33,14 +33,12 @@ void EntitySpawner::spawnWall(Vec2 position, Vec2 dimensions)
 {
     auto e = m_entityManager.addEntity(Crucible::EntityType::WALL);
 
-
-    sf::VertexArray vertices(sf::LinesStrip, 5);
-    vertices[0].position = sf::Vector2f(position.x - dimensions.x/2, position.y - dimensions.y/2);
-    vertices[1].position = sf::Vector2f(position.x + dimensions.x/2, position.y - dimensions.y/2);
-    vertices[2].position = sf::Vector2f(position.x + dimensions.x/2, position.y + dimensions.y/2);
-    vertices[3].position = sf::Vector2f(position.x - dimensions.x/2, position.y + dimensions.y/2);
-    vertices[4].position = sf::Vector2f(position.x - dimensions.x/2, position.y - dimensions.y/2);
-    //shape.setFillColor(sf::Color::Blue);
+    sf::VertexArray vertices(sf::Quads, 5);
+    vertices[0] = sf::Vertex({position.x - dimensions.x/2, position.y - dimensions.y/2}, sf::Color::Blue);
+    vertices[1] = sf::Vertex({position.x + dimensions.x/2, position.y - dimensions.y/2}, sf::Color::Blue);
+    vertices[2] = sf::Vertex({position.x + dimensions.x/2, position.y + dimensions.y/2}, sf::Color::Blue);
+    vertices[3] = sf::Vertex({position.x - dimensions.x/2, position.y + dimensions.y/2}, sf::Color::Blue);
+    vertices[4] = sf::Vertex({position.x - dimensions.x/2, position.y - dimensions.y/2}, sf::Color::Blue);
 
     e.addComponent<Component::CTransform>(position);
     e.addComponent<Component::CShape>(vertices);
